77050068bf9d847bfc75b8df40ecd05c0a9ffde8
jhzhou <jhzhou@localhost.localdomain>
2016-03-20 23:05:38 +0800

    delete useless code in user.py

--- jhzhou/user.py
+++ jhzhou/user.py
@@ -1,9 +1,4 @@
-'''
-Author:wepon
-Code:https://github.com/wepe
 
-File: cnn-svm.py
-'''
 from __future__ import print_function
 import cPickle
 import theano
@@ -15,32 +10,11 @@ import os
 from PIL import Image
 import numpy as np
 
-def svc(traindata,trainlabel,testdata,testlabel):
-    print("Start training SVM...")
-    svcClf = SVC(C=1.0,kernel="rbf",cache_size=3000)
-    svcClf.fit(traindata,trainlabel)
-    
-    pred_testlabel = svcClf.predict(testdata)
-    num = len(pred_testlabel)
-    accuracy = len([1 for i in range(num) if testlabel[i]==pred_testlabel[i]])/float(num)
-    print("cnn-svm Accuracy:",accuracy)
-
-def rf(traindata,trainlabel,testdata,testlabel):
-    print("Start training Random Forest...")
-    rfClf = RandomForestClassifier(n_estimators=400,criterion='gini')
-    rfClf.fit(traindata,trainlabel)
-    
-    pred_testlabel = rfClf.predict(testdata)
-    num = len(pred_testlabel)
-    accuracy = len([1 for i in range(num) if testlabel[i]==pred_testlabel[i]])/float(num)
-    print("cnn-rf Accuracy:",accuracy)
-
 def load_a_image(image_name):
     data = np.empty((1,1,28,28),dtype="float32")
     img = Image.open(image_name)
     arr = np.asarray(img, dtype= "float32")
     data[0,:,:,:] = arr
-    
     return data
 
 if __name__ == "__main__":
